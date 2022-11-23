2d111581b37c7b386f2aecba4a2ec6612cfd6a7e
Eunhye Kim <eunhyekim1223@gmail.com>
2022-11-23 09:58:03 +0000

    Fix _train_epoch to return log of metric(s)

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from ..logger import wandb_logger
 from . import loss, metric, optimizer, scheduler
+from ..utils import MetricTracker
 import wandb
 
 
@@ -32,9 +33,12 @@ class BaseTrainer:
 
         # 학습 관련 파라미터
         self.criterion = loss.get_loss(config)
-        self.metric_ftns = metric.get_metric(config)
+        self.metric_ftns = config.metric
         self.optimizer = optimizer.get_optimizer(self.model, config)
         self.lr_scheduler = scheduler.get_scheduler(self.optimizer, config)
+
+        self.train_metrics = MetricTracker('loss', *self.metric_ftns)
+        self.valid_metrics = MetricTracker('loss', *self.metric_ftns)
 
         self.epochs = config.epoch
         self.start_epoch = 1
@@ -48,18 +52,29 @@ class BaseTrainer:
 
         """
         할일
-        1. return에 wandb로 찍을 값 넘기기
+        1. return에 wandb로 찍을 값 넘기기 (11/23 김은혜 완)
         2. 최적의 경우 모델 parameter 저장해서 inference가 가능하도록 하기 (pytorch template 원래 파일 가면 예시가 잘 있다.)
         """
+        log = dict()
         self.model.train()
         for batch_idx, (data, target) in enumerate(self.train_data_loader):
             data, target = data.to(self.device), target.to(self.device)
 
-            self.optimizer.zero_grad()
+            
             output = self.model(data)
             loss = self.criterion(output, target)
+            self.train_metrics.update('loss', loss.item())
+            for met in self.metric_ftns:
+                ftns = metric.get_metric(met)
+                self.train_metrics.update(met, ftns(output, target))
+
+            # Backpropagation
+            self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+        
+        train_log = self.train_metrics.result()
+        log.update(**{'train_'+k : v for k, v in train_log.items()})
 
         self.model.eval()
         for batch_idx, (data, target) in enumerate(self.valid_data_loader):
@@ -68,10 +83,17 @@ class BaseTrainer:
             self.optimizer.zero_grad()
             output = self.model(data)
             loss = self.criterion(output, target)
+            self.valid_metrics.update('loss', loss.item())
+            for met in self.metric_ftns:
+                ftns = metric.get_metric(met)
+                self.valid_metrics.update(met, ftns(output, target))
+
+        val_log = self.valid_metrics.result()
+        log.update(**{'val_'+k : v for k, v in val_log.items()})
 
         self.lr_scheduler.step()
 
-        return loss
+        return log
 
     def train(self):
         """
@@ -83,14 +105,4 @@ class BaseTrainer:
         wandb_logger.init(key, self.model)
         for epoch in range(self.start_epoch, self.epochs + 1):
             result = self._train_epoch(epoch)
-            wandb.log(result)
-            wandb.log(
-                {
-                    "epoch": epoch,
-                    "train_loss_epoch": train_loss,
-                    "train_auc_epoch": train_auc,
-                    "train_acc_epoch": train_acc,
-                    "valid_auc_epoch": auc,
-                    "valid_acc_epoch": acc,
-                }
-            )
+            wandb.log(result, step=epoch)
