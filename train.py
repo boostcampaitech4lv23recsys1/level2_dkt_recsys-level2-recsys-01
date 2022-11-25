e146dd037823741cbd7e55cd5c779a5f3b795803
Eunhye Kim <eunhyekim1223@gmail.com>
2022-11-25 02:43:49 +0000

    Modify to set wandb sweep

--- train.py
+++ train.py
@@ -1,9 +1,43 @@
+import argparse
+
+from trainer import BaseTrainer
+from utils import read_json
+import model as models
+
+import wandb
+import torch
+
 """
 data 불러와서 trainer.py에 넘겨주기
 """
 
 
 def main(config):
-    data = dataloader()
-    trainer = trainer(data)
-    trainer.train()
+    train, valid = dataloader()
+    model = getattr(models, config['arch']['type'])
+    trainer = BaseTrainer(
+        model=model,
+        train_data_loader=train,
+        valid_data_loader=valid,
+        config=config,
+    )
+
+    if config['sweep']:
+        sweep_id = wandb.sweep(config['sweep'])
+        wandb.agent(sweep_id, trainer.train, count=1)
+    else:
+        trainer.train()
+
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='DKT Dinosaur')
+    args.add_argument('-c', '--config', default='./config.json', type=str,
+                      help='config 파일 경로 (default: "./config.json")')
+    args = args.parse_args()
+    config = read_json(args.config)
+    
+    config['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+
+    main(config)
