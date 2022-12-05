4ac4c3784fd98ee094f0afb4177fdf7ed98e242b
Myeonghyeon Ryu <tmxkdd5790@gmail.com>
2022-12-05 10:43:29 +0900

    Update and rename getmodel.py to get_model.py

--- model/get_model.py
+++ model/get_model.py
@@ -31,5 +31,19 @@ def get_model(config):
 
     if config["arch"]["type"] == "LSTM":
         model = getattr(models, config["arch"]["type"])(config).to(config["device"])
+        
+    if config["arch"]["type"] == "TransformerGRU":
+        model_config = config["arch"]["args"]
+        model = getattr(models, config["arch"]["type"])(
+            dim_model=model_config["dim_model"],
+            dim_ffn=model_config["dim_ffn"],
+            num_heads=model_config["num_heads"],
+            n_layers_transformer=model_config["n_layers_transformer"],
+            n_layers_LSTM=model_config["n_layers_LSTM"],
+            dropout_rate=model_config["dropout_rate"],
+            embedding_dim=model_config["embedding_dim"],
+            device=config["device"],
+            config=config,
+        ).to(config["device"])    
 
     return model
