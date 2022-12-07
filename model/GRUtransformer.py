18921d28971816b0e748b475557a7b821e71050d
Eunhye Kim <eunhyekim1223@gmail.com>
2022-12-07 02:02:53 +0000

    Add feature one embedding function

--- model/GRUtransformer.py
+++ model/GRUtransformer.py
@@ -2,7 +2,7 @@ from model import LSTM
 import torch.nn as nn
 import torch
 from model.layer.encoder import Encoder
-from model.utils import get_attn_mask, feature_embedding
+from model.utils import get_attn_mask, feature_embedding, feature_one_embedding
 
 
 class GRUtransformer(LSTM):
@@ -27,14 +27,23 @@ class GRUtransformer(LSTM):
         
     def forward(self, X):
         mask = X["mask"]
-        X = feature_embedding(
-            X=X,
-            cat_cols=self.cat_cols,
-            emb_cat_dict=self.emb_cat_dict,
-            cat_comb_proj=self.cat_comb_proj,
-            num_comb_proj=self.num_comb_proj,
-            device=self.device,
-        )
+        if self.one_embedding:
+            X = feature_one_embedding(
+                X=X,
+                cat_comb_proj=self.cat_comb_proj,
+                num_comb_proj=self.num_comb_proj,
+                emb_cat=self.emb_cat,
+                device=self.device
+            )
+        else:
+            X = feature_embedding(
+                X=X,
+                cat_cols=self.cat_cols,
+                emb_cat_dict=self.emb_cat_dict,
+                cat_comb_proj=self.cat_comb_proj,
+                num_comb_proj=self.num_comb_proj,
+                device=self.device,
+            )
 
         mask = get_attn_mask(mask).to(self.device)
 
