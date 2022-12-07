8c926caa09b9bad2345e0c7ae3726d6701683d80
ryubright <tmxkdd5790@gmail.com>
2022-12-08 00:05:53 +0900

    Add gtnGRU

--- model/__init__.py
+++ model/__init__.py
@@ -5,5 +5,7 @@ from .transformerLSTM import TransformerLSTM
 from .transformerGRU import TransformerGRU
 from .GRUtransformer import GRUtransformer
 from .GTN import GTN
+from .GtnGRU import GtnGRU
 
 from .get_model import get_models
+
