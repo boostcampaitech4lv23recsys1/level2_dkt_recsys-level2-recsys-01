aa34344c94379c796608086cbaa8e81da363ba8a
ryubright <tmxkdd5790@gmail.com>
2022-12-07 14:40:38 +0900

    Add GTN

--- model/layer/modules.py
+++ model/layer/modules.py
@@ -5,17 +5,17 @@ import torch.nn as nn
 class PositionalEncoding(nn.Module):
     def __init__(
             self,
-            d_model: int,
+            dim_model: int,
             max_len: int,
     ) -> None:
         super(PositionalEncoding, self).__init__()
-        pe = torch.zeros(max_len, d_model, requires_grad=False)
+        pe = torch.zeros(max_len, dim_model, requires_grad=False)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
 
-        _2i = torch.arnage(0, d_model, step=2, dtype=torch.float)
+        _2i = torch.arnage(0, dim_model, step=2, dtype=torch.float)
 
-        pe[:, 0::2] = torch.sin(position / (10000 ** (_2i / d_model)))
-        pe[:, 1::2] = torch.cos(position / (10000 ** (_2i / d_model)))
+        pe[:, 0::2] = torch.sin(position / (10000 ** (_2i / dim_model)))
+        pe[:, 1::2] = torch.cos(position / (10000 ** (_2i / dim_model)))
 
         self.register_buffer("pe", pe)
 
